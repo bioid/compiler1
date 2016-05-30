1cce51f5ce0d9a84eeb563d7cd5ae2e8d4145a0c
Brandon Hinshaw <bkhinshaw@gmail.com>
2016-05-30 09:12:39 +0000

    testing

--- ast/main.cc
+++ ast/main.cc
@@ -1,9 +1,29 @@
+#include <cstdio>
 #include "NodeTree.h"
+
+using namespace std;
+
+void listChildren(Node* node);
+void traverse(NodeTree* nt);
 
 int main() 
 {
 	NodeTree nt;
 	nt.addOperator(ADD, INTEGER, INTEGER, 1, 2);
+	
+	listChildren(nt.root);
 	return 0; 
 }
+
+void listChildren(Node* node)
+{	
+	list<Node*>::iterator itr = node->children.begin();
+	printf("Node type %d has %d children\n", node->nodeType, (int)node->children.size());
+	while (itr != node->children.end()) 
+	{
+		listChildren(*itr);
+		itr++;
+	}
+}
+
 
